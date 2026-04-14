7d23fa72dc1776c91543a676a27d8e7d64aee96e
Nataliia <natalia.gerbeeva@gmail.com>
2026-04-14 20:13:11 +0300

    Домашнее задание по массивам готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,31 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(function(user) {
+        return user.gender === gender;
+    });
+
+    if (filteredUsers.length === 0) {
+        return 0;
+    }
+
+    let sumAge = 0;
+
+    for (let i = 0; i < filteredUsers.length; i++) {
+        sumAge += filteredUsers[i].age;
+    }
+
+    return sumAge / filteredUsers.length;
 }
