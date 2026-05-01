0ef4667e681ffa6dd88c86ccaf4c542aa3854edf
Nataliia <natalia.gerbeeva@gmail.com>
2026-05-01 10:02:50 +0300

    Домашнее задание по классам готово

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -89,3 +89,45 @@ class Library {
     return null;
   }
 }
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.marks = {};
+  }
+
+  addMark(mark, subject) {
+    if (mark < 2 || mark > 5) {
+      return;
+    }
+
+    if (!this.marks.hasOwnProperty(subject)) {
+      this.marks[subject] = [];
+    }
+
+    this.marks[subject].push(mark);
+  }
+
+  getAverageBySubject(subject) {
+    if (!this.marks.hasOwnProperty(subject)) {
+      return 0;
+    }
+
+    const sum = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+
+    return sum / this.marks[subject].length;
+  }
+
+  getAverage() {
+    const subjects = Object.keys(this.marks);
+
+    if (subjects.length === 0) {
+      return 0;
+    }
+
+    const sum = subjects.reduce((acc, subject) => {
+      return acc + this.getAverageBySubject(subject);
+    }, 0);
+
+    return sum / subjects.length;
+  }
+}
