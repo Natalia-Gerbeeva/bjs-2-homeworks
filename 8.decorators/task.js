00dad2cecb4c6a81242ed24fc4ff05aff8bc71cb
Nataliia <natalia.gerbeeva@gmail.com>
2026-05-01 10:57:40 +0300

    Домашнее задание по декораторам выполнено

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  const cache = new Map();
+
+  return function(...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      return "Из кеша: " + cache.get(key);
+    }
+
+    const result = func(...args);
+
+    if (cache.size >= 5) {
+      const firstKey = cache.keys().next().value;
+      cache.delete(firstKey);
+    }
+
+    cache.set(key, result);
+
+    return "Вычисляем: " + result;
+  };
 }
 
-//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+  let timeoutId = null;
+  let isFirstCall = true;
+
+  function wrapper(...args) {
+    wrapper.allCount++;
+
+    if (isFirstCall) {
+      func.apply(this, args);
+      wrapper.count++;
+      isFirstCall = false;
+      return;
+    }
+
+    clearTimeout(timeoutId);
+
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+      wrapper.count++;
+    }, delay);
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  return wrapper;
 }
